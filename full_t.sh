e003a0307c522cdc8d2aa71fb14271f9f3c36249
Tozuck <122993981+Tozuck@users.noreply.github.com>
2024-07-12 16:51:23 +0330

    Update full_t.sh

--- full_t.sh
+++ full_t.sh
@@ -86,7 +86,9 @@ sudo apt install -y apache2
 sudo touch /var/www/html/file.txt
 sudo chown www-data:www-data /var/www/html/file.txt
 sudo chmod 664 /var/www/html/file.txt
-
+sleep 2
+cd full_t
+sleep 2
 # Install Python 3 venv and create a virtual environment
 sudo apt install -y python3-venv
 python3 -m venv myenv
