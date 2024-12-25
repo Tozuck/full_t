215df451c6b45bb45c8ccbc66865b1456b822438
Tozuck <122993981+Tozuck@users.noreply.github.com>
2024-12-25 16:51:26 +0330

    Update start_monitor.sh

--- start_monitor.sh
+++ start_monitor.sh
@@ -1,8 +1,1 @@
-# Navigate to the directory where the virtual environment and monitor.py are located
-cd ~/full_t
 
-# Activate the virtual environment
-source myenv/bin/activate
-
-# Run monitor.py in the background
-nohup python monitor.py > /var/log/monitor.log 2>&1 &
